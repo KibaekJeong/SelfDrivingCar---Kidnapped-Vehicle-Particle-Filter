5351342a39747d248a9525a965096aafaccba276
kibaek jeong <jgibak@gmail.com>
2020-03-23 14:52:44 +0900

    fix

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -28,8 +28,8 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    *   first position (based on estimates of x, y, theta and their uncertainties
    *   from GPS) and all weights to 1.
    * Add random Gaussian noise to each particle.
-
-    num_particles = 50;  // TODO: Set the number of particles
+   */
+    num_particles = 50;
     default_random_engine gen;
     normal_distribution<double> dist_x(x,std[0]);
     normal_distribution<double> dist_y(y,std[1]);
